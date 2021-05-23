67ec060c1758fde565458a4b9459190444f5d1a0
alexandr <sashkaageev@gmail.com>
2021-05-24 00:27:01 +0300

    Added Factory Method

---
+++ DotnetDesignPatterns.Creational.Factories/Point.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace DotnetDesignPatterns.Creational.Factories
+{
+    public class Point
+    {
+        private double x, y;
+
+        private Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static Point NewCartesianPoint(double x, double y)
+        {
+            return new Point(x, y);
+        }
+
+        public static Point NewPolarPoint(double rho, double theta)
+        {
+            return new Point(rho * Math.Cos(theta), rho + Math.Sin(theta));
+        }
+    }
+}
---
+++ DotnetDesignPatterns.Creational.Factories/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotnetDesignPatterns.Creational.Factories
+{
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var p = Point.NewCartesianPoint(2, 5);
+        }
+    }
+}
