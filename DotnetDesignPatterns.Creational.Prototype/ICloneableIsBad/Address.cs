d04aba3c8cea36f5c96a66662c96e2f31dc0c231
alexandr <sashkaageev@gmail.com>
2021-05-26 17:10:45 +0300

    ICloneable example for Prototype

---
+++ DotnetDesignPatterns.Creational.Prototype/ICloneableIsBad/Address.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace DotnetDesignPatterns.Creational.Prototype.ICloneableIsBad
+{
+    public class Address : ICloneable
+    {
+        public string StreetName;
+        public int HouseNumber;
+
+        public Address(string streetName, int houseNumber)
+        {
+            StreetName = streetName;
+            HouseNumber = houseNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(StreetName)} : {StreetName};" +
+                   $" {nameof(HouseNumber)} : {HouseNumber}";
+        }
+
+        public object Clone()
+        {
+            return new Address(StreetName, HouseNumber);
+        }
+    }
+}
---
+++ DotnetDesignPatterns.Creational.Prototype/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using DotnetDesignPatterns.Creational.Prototype.ICloneableIsBad;
+
+namespace DotnetDesignPatterns.Creational.Prototype
+{
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var john = new Person(
+                new[] {"John", "Smith"},
+                new Address("London Road", 123));
+
+            var jane = (Person) john.Clone();
+            jane.Names[0] = "Jane";
+            jane.Address.HouseNumber = 333;
+            
+            Console.WriteLine(john);
+            Console.WriteLine(jane);
+        }
+    }
+}
